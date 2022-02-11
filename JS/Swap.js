d2252f7ea3f30de61abf426159cd4f870393a744
cmdtrwd <99168914+cmdtrwd@users.noreply.github.com>
2022-02-11 10:10:49 +0700

    Add files via upload

---
+++ JS/Swap.js
@@ -0,0 +1,65 @@
+function changeImage() {
+    var image = document.getElementById('mahiru');
+    if (image.src.match("bulbon")) {
+        image.src = "Images/mahiru.png";
+    } 
+    else
+        {
+        image.src = "Images/mahiru_z.png";
+    }
+}
+
+function changeImage2() {
+    var image = document.getElementById('kotoko');
+    if (image.src.match("bulbon")) {
+        image.src = "Images/kotoko.png";
+    } 
+    else
+        {
+        image.src = "Images/kotoko_z.png";
+    }
+}
+
+function changeImage3() {
+    var image = document.getElementById('mikoto');
+    if (image.src.match("bulbon")) {
+        image.src = "Images/Mikoto.png";
+    } 
+    else
+        {
+        image.src = "Images/Mikoto_z.png";
+    }
+}
+
+function changeback() {
+    var image = document.getElementById('mahiru');
+    if (image.src.match("bulbon")) {
+        image.src = "Images/mahiru_z.png";
+    } 
+    else
+        {
+        image.src = "Images/mahiru.png";
+    }
+}
+
+function changeback2() {
+    var image = document.getElementById('kotoko');
+    if (image.src.match("bulbon")) {
+        image.src = "Images/kotoko_z.png";
+    } 
+    else
+        {
+        image.src = "Images/kotoko.png";
+    }
+}
+
+function changeback3() {
+    var image = document.getElementById('mikoto');
+    if (image.src.match("bulbon")) {
+        image.src = "Images/Mikoto_z.png";
+    } 
+    else
+        {
+        image.src = "Images/Mikoto.png";
+    }
+}
